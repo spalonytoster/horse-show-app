fcae0e224f837a551ff2ef7b614c6fc58de896d5
Maciej Posłuszny <xmachiei@gmail.com>
2016-06-05 21:30:09 +0200

    adding group tabs

--- ng/Admin/GroupsCtrl.js
+++ ng/Admin/GroupsCtrl.js
@@ -1,6 +1,8 @@
 angular.module('App.Admin')
   .controller('GroupsCtrl', function ($scope) {
 
+    $scope.tabIndex = 0;
+
     $scope.tabs = [{
       label: 'Group A',
       refrees: [],
@@ -16,12 +18,23 @@ angular.module('App.Admin')
     }];
 
     $scope.addGroup = function () {
+      var label;
+      if ($scope.groupTitle) {
+        label = $scope.groupTitle;
+      }
+      else {
+        label = 'Group ' + String.fromCharCode($scope.tabs.length + 65);
+      }
       $scope.tabs.push({
-        label: 'Group ' + String.fromCharCode($scope.tabs.length + 65),
+        label: label,
         refrees: [],
         contestants: []
       });
+      $scope.groupTitle = '';
     };
 
+    $scope.removeCurrentGroup = function () {
+      $scope.tabs.splice($scope.tabIndex, 1);
+    };
 
   });
